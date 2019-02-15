22b2925c9d42e42af5ff7687facde7dce1092cbf
mrewers <mrewers@users.noreply.github.com>
2019-02-15 14:31:25 -0500

    Hook up block toggle to the admin page

--- admin/js/admin-interface/AdminPage.jsx
+++ admin/js/admin-interface/AdminPage.jsx
@@ -1,6 +1,17 @@
+import SelectionPage from './SelectionPage';
+import defaultBlocks from './utils/defaultBlocks.json';
+
+const { blockType } = defaultBlocks;
+
 const AdminPage = () => (
   <div className="iip-gut-admin-page">
     <h1>Configure Your IIP Gutenblocks</h1>
+    { blockType.map( type => (
+      <SelectionPage
+        blockGroup={ type.blocks }
+        groupName={ type.group }
+      />
+    ) ) }
   </div>
 );
 
