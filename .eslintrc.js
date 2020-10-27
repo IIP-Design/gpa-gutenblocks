e3275c21e42542ef6c2ac9c19cd62c16b0fb3c34
mrewers <mrewers@users.noreply.github.com>
2020-10-27 17:16:02 -0400

    fix: exempt prop-types from no-published imports rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,11 @@ module.exports = {
   'extends': ['@gpa-lab/eslint-config', '@gpa-lab/eslint-config/react'],
   ignorePatterns: ['dist/', 'vendor/'],
   parser: 'babel-eslint',
+  rules: {
+    'node/no-unpublished-import': [
+      'error', {
+        allowModules: ['prop-types'],
+      },
+    ],
+  },
 };
